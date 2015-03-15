0827baba5680473c4b3cb1426e92b851fd7c12f7
WispProxy <Victor@WispProxy.ru>
2015-03-15 12:27:55 +0300

    Release. 0.1.201503151227. Final prototype.

--- app/models/student.rb
+++ app/models/student.rb
@@ -42,45 +42,132 @@ class Student < ActiveRecord::Base
     #
     # self.find_by_sql(students)
 
+    #TEST-1# Need optimization
+    # students = <<-SQL
+    #   SELECT	students.id,
+    #           students.name AS students_name,
+    #           students.surname AS students_surname,
+    #           students.date_of_birth AS students_date_of_birth,
+    #           students.email AS students_email,
+    #           students.registration_ip AS students_registration_ip,
+    #           /*students.registration_time AS students_registration_time,*/
+    #
+    #           /*ratings.rating AS ratings_rating,*/
+    #           (
+    #           SELECT ROUND(AVG(ratings.rating), 2) AS average_ratings
+    #           FROM	ratings
+    #           WHERE	ratings.student_id = students.id
+    #           ),
+    #
+    #           /*subjects.name AS subjects_name,*/
+    #
+    #           groups.semester AS groups_semester,
+    #           groups.name AS groups_name
+    #
+    #   FROM	  students,
+    #           ratings,
+    #           subjects,
+    #           groups
+    #
+    #   WHERE	  groups.id = students.group_id
+    #           AND
+    #           students.id = ratings.student_id
+    #           AND
+    #           subjects.id = ratings.subject_id
+    #           AND
+    #           ratings.rating IS NOT NULL
+    #
+    #   ORDER BY
+			#         average_ratings DESC
+    #
+    #   LIMIT 10 ;
+    # SQL
+    #
+    # self.find_by_sql(students)
+
+
+    #TEST-2# OUTPUT ALL DATAS
+    # students = <<-SQL
+    #   SELECT DISTINCT
+    #     groups.id AS groups_id,
+    #     groups.name AS groups_name,
+    #     groups.semester AS groups_semester,
+    #     subjects.id AS subjects_id,
+    #     subjects.name AS subjects_name,
+    #     ROUND(AVG(ratings.rating), 2) AS average_ratings,
+    #     ratings.subject_id AS ratings_subject_id,
+    #     ratings.student_id AS ratings_student_id,
+    #     ratings.id AS ratings_id,
+    #     students.id AS students_id,
+    #     students.name AS students_name,
+    #     students.surname AS students_surname,
+    #     students.group_id AS students_group_id,
+    #     students.date_of_birth AS students_date_of_birth,
+    #     students.email AS students_email,
+    #     students.registration_ip AS students_registration_ip,
+    #     students.registration_time AS students_registration_time,
+    #     students.characteristic AS students_characteristic
+    #   FROM
+    #     ratings
+    #     INNER JOIN students ON (ratings.student_id = students.id)
+    #     INNER JOIN groups ON (students.group_id = groups.id)
+    #     INNER JOIN subjects ON (ratings.subject_id = subjects.id)
+    #   GROUP BY
+    #     groups_id,
+    #     groups_name,
+    #     groups_semester,
+    #     subjects_id,
+    #     subjects_name,
+    #     ratings_subject_id,
+    #     ratings_student_id,
+    #     ratings_id,
+    #     students_id,
+    #     students_name,
+    #     students_surname,
+    #     students_group_id,
+    #     students_date_of_birth,
+    #     students_email,
+    #     students_registration_ip,
+    #     students_registration_time,
+    #     students_characteristic
+    #   ORDER BY
+    #     average_ratings DESC
+    #   LIMIT 10 ;
+    # SQL
+    #
+    # self.find_by_sql(students)
+
+
+    #TEST-3# THIS SQL-query - WORKED!
     # Need optimization and convert to Active Record
     students = <<-SQL
-      SELECT 	students.id,
-              students.name,
-              students.surname,
-              students.date_of_birth,
-              students.email,
-              students.registration_ip,
-              /*students.registration_time,*/
-
-              /*ratings.rating,*/
-              (
-                SELECT	ROUND(AVG(ratings.rating), 2) AS average_rating
-                FROM	ratings
-                WHERE	ratings.student_id = students.id
-              ),
-
-              /*subjects.name,*/
-
-              groups.semester
-
-      FROM	  students,
-              ratings,
-              subjects,
-              groups
-
-      WHERE	  groups.id = students.group_id
-              AND
-              students.id = ratings.student_id
-              AND
-              subjects.id = ratings.subject_id
-              AND
-              ratings.rating IS NOT NULL
-
-      ORDER BY average_rating DESC LIMIT 10 ;
+      SELECT DISTINCT
+        students.name AS students_name,
+        students.surname AS students_surname,
+        groups.semester AS groups_semester,
+        groups.name AS groups_name,
+        students.email AS students_email,
+        ROUND(AVG(ratings.rating), 2) AS average_ratings
+      FROM
+        ratings
+        INNER JOIN students ON (ratings.student_id = students.id)
+        INNER JOIN groups ON (students.group_id = groups.id)
+        INNER JOIN subjects ON (ratings.subject_id = subjects.id)
+        INNER JOIN ratings ratings1 ON (students.id = ratings1.student_id)
+        AND (subjects.id = ratings1.subject_id)
+        INNER JOIN groups groups1 ON (students.group_id = groups1.id)
+      GROUP BY
+        students_name,
+        students_surname,
+        groups_semester,
+        groups_name,
+        students_email
+      ORDER BY
+        average_ratings DESC
+      LIMIT 10 ;
     SQL
 
     self.find_by_sql(students)
-
 
     # Experiments with Active Record
     # for optimization sql-query
